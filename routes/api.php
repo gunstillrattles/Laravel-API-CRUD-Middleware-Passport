1a04da6644b4fd5bca8b5177610831e6083fb01c
gunstillrattles <marlen.mukashev@narxoz.kz>
2022-12-16 18:49:00 +0600

    Initial commit

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::group(['prefix' => 'user'], function () {
+    Route::post('/register', [\App\Http\Controllers\API\AuthController::class, 'register']);
+    Route::post('/login', [\App\Http\Controllers\API\AuthController::class, 'login']);
+});
+
+
+Route::group(['prefix' => 'emps'], function () {
+    Route::post('/store', [\App\Http\Controllers\API\EmployeeController::class, 'store'])
+        ->middleware(['auth:api', 'admin']);
+    Route::get('/', [\App\Http\Controllers\API\EmployeeController::class, 'index'])->middleware(['auth:api']);
 });
